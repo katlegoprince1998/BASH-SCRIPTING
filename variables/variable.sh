52223c8d682b21612563760eb177f9fe4e16b5a8
katlegoprince1998 <princekatlego357@gmail.com>
2024-01-12 11:35:49 +0200

    Created a simple scripting to learn more about variables in bash

---
+++ variables/variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# To access contents of a varilble we use dollar sign before the name of the variable
+#
+myname="Katlego"
+age="25"
+workingdir=l
+
+echo "My name is $myname "
+echo "I am $age years old"
+echo workingdir
+
